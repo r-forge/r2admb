b2be315a93c4a5cf321ddf1a53c7d54ef4a796e1
bbolker (Ben  Bolker) <bolker@mcmaster.ca>
2011-09-29 22:06:34 +0000

    fixed glitch (I hope)
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/r2admb@66 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -1,11 +1,14 @@
 
-<!-- This is the project specific website template --><!-- It can be changed as liked or replaced by other content --><?php 
+<!-- This is the project specific website template --><!-- It can be changed as liked or replaced by other content -->
+
+
+<?php 
 
 $domain=ereg_replace('[^\.]*\.(.*)$','\1',$_SERVER['HTTP_HOST']);
 $group_name=ereg_replace('([^\.]*)\..*$','\1',$_SERVER['HTTP_HOST']);
 $themeroot='http://r-forge.r-project.org/themes/rforge/';
 
-echo '<?xml version="1.0" encoding="UTF-8"?>;
+echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en   "><head>  
